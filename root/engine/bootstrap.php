bfd15474ab0ae64d1fc1941df52b32a8ae394eee
Rick Hanson <git.commit@rick-hanson.net>
2014-11-17 20:57:59 +0000

    Initial root commit

---
+++ root/engine/bootstrap.php
@@ -0,0 +1,13 @@
+<?php
+/*
+PROJECT KATANA
+==============
+File: /engine/bootstrap.php
+Purpose: Engine bootstrapper
+Revision: 1
+Last edit: 2014-11-17 20:02
+
+Changelog:
+r1: Initial build
+*/
+?>
---
+++ root/webroot/index.php
@@ -0,0 +1,14 @@
+<?php
+/*
+PROJECT KATANA
+==============
+File: /webroot/index.php
+Purpose: Engine index file
+Revision: 1
+Last edit: 2014-11-17 20:07
+
+Changelog:
+r1: Initial build
+*/
+require_once('../engine/bootstrap.php');
+?>
